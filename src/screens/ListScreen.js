2151c5de38af7a3f104252a379df48b7841e47ab
Harish Lalwani <harish.lalwani@walmart.com>
2022-05-31 19:12:13 +0530

    Flat List Primitive type

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,31 +3,36 @@ import { View, Text, FlatList, StyleSheet } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend #1" },
-    { name: "Friend #2" },
-    { name: "Friend #3" },
-    { name: "Friend #4" },
-    { name: "Friend #5" },
-    { name: "Friend #6" },
-    { name: "Friend #7" },
-    { name: "Friend #8" },
-    { name: "Friend #9" },
-    { name: "Friend #10" },
+    { name: "Friend #1" , age:"12"},
+    { name: "Friend #2" , age:"13"},
+    { name: "Friend #3" , age:"14"},
+    { name: "Friend #4" , age:"15"},
+    { name: "Friend #5" , age:"16"},
+    { name: "Friend #6" , age:"17"},
+    { name: "Friend #7" , age:"18"},
+    { name: "Friend #8" , age:"19"},
+    { name: "Friend #9" , age:"22"},
+    { name: "Friend #10", age:"23" },
   ];
   return (
     <FlatList
+      // horizontal -- Our list will be displayed horizontal
+    //   horizontal
+    //   showsHorizontalScrollIndicator={true}
+      keyExtractor={(friend) => friend.name}
       data={friends}
       // element is like  {item: {name: "Friend #1"}, index:0}
       // so rather then using whole element we destructure it to use just item
       renderItem={({ item }) => {
-        return <Text style={styles.listStyle}>{item.name}</Text>;
+          return <Text style={styles.textStyle}>{item.name} - Age : { item.age}</Text>;
       }}
     />
   );
 };
 
 const styles = StyleSheet.create({
-  listStyle: {
+  textStyle: {
+    marginVertical: 50,
     fontSize: 30,
   },
 });
